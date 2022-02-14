f13540d6ad958ccb5ce100b3c2eccefd858c0130
Cristina <cristina.gratton@gmail.com>
2022-02-14 15:57:44 -0600

    first commit of events

---
+++ src/main/java/org/launchcode/codingspring/CodingSpringApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/codingspring/CodingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
